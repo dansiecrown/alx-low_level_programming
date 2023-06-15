639f07e1969ed2341ffd0c354e097cd0105b1dd3
dansiecrown <dansiecrown@gmail.com>
2023-06-15 16:04:50 -0700

    another bug fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	int s=strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+		  strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 
-  return (1);
+	return (1);
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	      s);
