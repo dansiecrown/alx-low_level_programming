4e7def322fe11f8052896c4536e8fc63ae296a79
dansiecrown <dansiecrown@gmail.com>
2023-07-19 16:38:27 -0700

    tried another method

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,20 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - Prints out a name.
- * @name: The name to be printed.
- * @f: A pointer to the function.
- *
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is pointer
  * Return: Nothing.
- */
-
+**/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-        	f(name);
+if (name == NULL || f == NULL)
+{
+return;
+}
+else
+{
+f(name);
+}
 }
